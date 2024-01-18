ff8d1e8086518dad53a8d246f170171380085fb0
Yuri Luis <71914185+yuriluisz@users.noreply.github.com>
2024-01-18 04:51:38 -0300

    PROJETO INICIAL
    Projeto inicial apenas pra termos uma noção e apresentar pro client uma ideia dos planos

---
+++ Codigo/script.js
@@ -0,0 +1,5 @@
+function ObterValores(){
+    var emailInput = document.getElementById("email").value;
+    var senhaInput = document.getElementById("senha").value;
+    if (emailInput == "adm", senhaInput == "adm") alert("Senha Correta!")
+}

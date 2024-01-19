17f532163dea3d78c1b504643c7196f80eae3b42
Yuri Luis <71914185+yuriluisz@users.noreply.github.com>
2024-01-19 02:34:27 -0300

    testes github pages

--- Codigo/script.js
+++ Codigo/script.js
@@ -1,5 +1,19 @@
-function ObterValores(){
-    var emailInput = document.getElementById("email").value;
-    var senhaInput = document.getElementById("senha").value;
-    if (emailInput == "adm", senhaInput == "adm") alert("Senha Correta!")
-}
+document.addEventListener("DOMContentLoaded", () => {
+    var erroLogin = document.getElementById("erroUsuario");
+    const botaoEntrar = document.querySelector("#usuarioAcessar");
+    
+    botaoEntrar.addEventListener("click", () => {
+        console.log("Redirecionando...")
+            var valorEmail = document.getElementById("loginUsuario").value;
+            var valorSenha = document.getElementById("senhaUsuario").value;
+            
+            if (valorEmail === "adm" && valorSenha === "adm"){
+                console.log*("Logado com sucesso");
+                window.location.href = "inicio.html"
+            }
+            else{
+                console.log("Email ou Senha Incorretos, Tente Novamente.");
+                document.getElementById("erroUsuario").innerText = "Email ou Senha Incorretos, Tente Novamente.";
+            };
+    });
+  });
